068e39685a69238d04cd89cbac0af09691c17970
Fernando Zigunov <fzigunov@gmail.com>
2024-07-14 13:51:09 -0400

    Added Executable
    Create
    
    Add files via upload
    
    Delete OmniCUDA_OneShot/x64/Release/
    
    Create Test.txt
    
    Add files via upload
    
    Added executables
    
    Delete OmniCUDA_Iterative/x64/Release directory
    
    Delete OmniCUDA_OneShot/x64/Release directory
    
    See Releases folder for binaries
    
    Uploaded R03
    
    New kernel ('cell-centered'); fixed memory leak; added CG restart
    
    Uploaded R03
    
    New kernel ('cell-centered'); fixed memory leak; added CG restart

--- OmniMatlab/example/Test_Taylor3D.m
+++ OmniMatlab/example/Test_Taylor3D.m
@@ -1,30 +1,3 @@
-% © 2024. Triad National Security, LLC. All rights reserved.
-% This program was produced under U.S. Government contract
-% 89233218CNA000001 for Los Alamos National Laboratory (LANL), which is
-% operated by Triad National Security, LLC for the U.S. Department of
-% Energy/National Nuclear Security Administration. All rights in the
-% program are reserved by Triad National Security, LLC, and the U.S.
-% Department of Energy/National Nuclear Security Administration. The
-% Government is granted for itself and others acting on its behalf a
-% nonexclusive, paid-up, irrevocable worldwide license in this material
-% to reproduce, prepare. derivative works, distribute copies to the
-% public, perform publicly and display publicly, and to permit
-% others to do so.
-%
-% This program is free software: you can redistribute it and/or modify it
-% under the terms of the GNU General Public License as published by the
-% Free Software Foundation, either version 3 of the License, or (at your
-% option) any later version.
-%
-% This program is distributed in the hope that it will be useful, but
-% WITHOUT ANY WARRANTY; without even the implied warranty of
-% MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
-% General Public License for more details.
-%
-% You should have received a copy of the GNU General Public License along
-% with this program. If not, see <https://www.gnu.org/licenses/>.
-
-
 clear; clc; close all;
 
 %Generates a Taylor vortex according to Charonko 2010 DOI 10.1088/0957-0233/21/10/105401 3D version
@@ -39,7 +12,7 @@ U0=nu/sqrt(H); %Characteristic Velocity, m/s
 T0=L0/U0; %Characteristic Time, s
 P0=rho*U0^2; %Characteristic Pressure, Pa
 
-NptsX=101; NptsY=NptsX; NptsZ=NptsX;
+NptsX=201; NptsY=NptsX; NptsZ=NptsX;
 
 x=linspace(-2*L0,2*L0,NptsX); y=linspace(-2*L0,2*L0,NptsY); z=linspace(-2*L0,2*L0,NptsZ);
 dx=x(2)-x(1); dy=y(2)-y(1); dz=z(2)-z(1);
@@ -66,19 +39,25 @@ Sx=(-rho*(dudt+Ux.*DUDX+Uy.*DUDY))/(P0/L0);
 Sy=(-rho*(dvdt+Ux.*DVDX+Uy.*DVDY))/(P0/L0);
 Sz=zeros(size(Sx));
 
-
-%% ----THIS IS WHERE THE OSMODI CODE IS USED----
-% Make sure the OSMODI.mexw64 file is inside this folder or inside of a Matlab search folder
-
 opts.SolverToleranceRel=1e-4;
 opts.SolverToleranceAbs=1e-6;
 opts.SolverDevice='GPU';
+%opts.Kernel='face-crossing';
+opts.Kernel='cell-centered';
 opts.Verbose=1;
+
+M=zeros(size(Sx));
+M(26:40,26:40,26:40)=1; %Makes sure the degeneracies are handled correctly
+M=M & rand(size(Sx))>0.5;
+Sx(M)=nan;
 
 disp('Starting OSMODI...')
 tic
-[P_OSMODI, CGS]=OSMODI(single(Sx),single(Sy),single(Sz),single([dx dy dz]/L0),opts);
+%[P_OSMODI, CGS]=OSMODI(single(Sx),single(Sy),single(Sz),single([dx dy dz]/L0),opts);
+[P_OSMODI, CGS]=OSMODI(Sx,Sy,Sz,[dx dy dz]/L0,opts);
 toc
+
+%figure; imagesc(squeeze(P_OSMODI(:,ceil(NptsX/2),:)));colorbar
 
 disp(['Time Taken for just GPU computation: ' num2str(CGS(end,3)-CGS(1,3)) 's'])
 
@@ -105,8 +84,9 @@ Err=(P_OSMODI - Ptruth)./NormPmax;
 ErrorPercent=100*sqrt(sum(Err(:).^2)./numel(Err));
 disp(['Error = ' num2str(ErrorPercent, '%0.3f') '%'])
 
-figure('color','w','Position', [1.3538e+03 530.6000 560 420]); semilogy(CGS(:,1), CGS(:,2))
-title('CG solver convergence')
+% figure('color','w','Position', [1.3538e+03 530.6000 560 420]); semilogy(CGS(:,1), CGS(:,2))
+% title('CG solver convergence')
 
-pause(2)
+% pause(2)
 
+clear;
